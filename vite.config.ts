a6f4f5be7e2f156d90a1e0806d62f17c9b1dff35
prasang03 <prasangjain03@gmail.com>
2026-09-18 16:20:53 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base:'/Indrakamal-Uniforms/'
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
